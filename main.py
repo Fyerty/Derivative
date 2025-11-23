16895fda4c28d5ff65cf08a3b54727af76d10950
Fyerty <yurabeimler@gmail.com>
2025-11-23 15:09:53 +0500

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,30 @@ from string import digits
 "-5x^4 - 5 x ^ 2 - 4"
 "5  *x^3 - 3 x ^ 2+1"
 "-5x - 5 x ^ 2 - 4"
+"-5x - 5x^2 - 4x^0.5"
 
+
+def preparation(exp: str) -> list:
+    exp = exp.replace(" ", "").replace("-", "+-")
+
+    for dig in digits:
+        exp = exp.replace(f"{dig}x", f"{dig}*x")
+
+    exp = exp.split("+")
+
+    return exp
+
+
+def flint(a: float|int|str):
+    if type(a) == float:
+        if float(int(a)) - float(a) == 0.0:
+            return int(a)
+        return a
+
+    elif type(a) == int:
+        return a
+
+    return flint(float(a))
 
 def deriv_polynomial(polynom: str) -> str:
     monoms = polynom.split("+")
@@ -14,7 +37,7 @@ def deriv_polynomial(polynom: str) -> str:
             u = monom.split("*")
             v = u[1].split("^")
             if len(v) == 1: v += [1]
-            deriv += f"+{str(int(u[0]) * int(v[1]))}x^{int(v[1]) - 1}"
+            deriv += f"+{flint(float(u[0]) * float(v[1]))}x^{flint(v[1]) - 1}"
         except IndexError:  ...
 
     deriv = deriv.replace("+-", "-").replace("^1", "").replace("x^0", "")
@@ -22,9 +45,4 @@ def deriv_polynomial(polynom: str) -> str:
     return deriv if deriv[0] == "-" else deriv.replace("+-", "-")[1:]
 
 
-exp = input("Введите многочлен: ").replace(" ", "").replace("-", "+-")
-
-for dig in digits:
-    exp = exp.replace(f"{dig}x", f"{dig}*x")
-
-print("Производная: " + deriv_polynomial(exp))
+print("Производная: " + deriv_polynomial(preparation(input("Введите выражение: "))))
